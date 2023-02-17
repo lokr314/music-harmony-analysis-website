aa6c0a3fe722935e8be9a252bb5c79e955443c93
Louis Krüger <lokr@uni-bremen.de>
2023-02-17 17:33:17 +0100

    Snipcart, Page Structure, Images... I should update my work more regularly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), vue()],
+  site: 'https://notenentwickler.com',
+  server: { port: 3000}
 });
