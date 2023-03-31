b954ef19213d4c63537111064d91e351b1ace8aa
Louis Krüger <lokr@uni-bremen.de>
2023-03-31 16:51:11 +0200

    Remove Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind()],
   site: 'https://notenentwickler.com',
   server: { port: 3000},
   //output: 'server'
